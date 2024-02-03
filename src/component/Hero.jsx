d6bfd72ebb038c4520e58763ff88459259e0440a
RaRAshed <rnrashedrn@gmail.com>
2024-02-03 10:11:18 -0800

    formsubmit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,4 +1,11 @@
 const Hero = (props) => {
+          function Demo() {
+                        alert('click');
+                  }
+    const PostFormData = (event) => {
+        event.preventDefault();
+             alert('form submitted')
+         }
     return (
         <div>
             <h1>{props.item['name']}</h1>
@@ -6,7 +13,18 @@ const Hero = (props) => {
             <h1>{props.item['city']}</h1>
             <div>
                 <button onClick={props.btn}>Click</button>
-           </div>
+            </div>
+             <div>
+                <button onClick={
+              Demo
+                }>Event click</button>
+            </div>
+            <div>
+                <form onSubmit={PostFormData}>
+                    <input type='text' placeholder="write some thing"/>
+                    <button type="submit"> form Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
