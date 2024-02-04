c3997fbaada904561b14f1c7ea6fe00ac5b5d193
RaRAshed <rnrashedrn@gmail.com>
2024-02-04 09:34:32 -0800

    react hook useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import Hero from './component/Hero';
 
 const App = () => {
@@ -10,10 +11,47 @@ const App = () => {
   const BtnClick = () => {
     alert('Pass function with funtion');
   }
+  let myHeadline = useRef();
+  let myImg = useRef();
+  let firstName, lastName = useRef();
+  let myCss = useRef();
+  const change = () => {
+      myHeadline.current.innerText = "Hello useRef";
+  }
+  const changeImg = () => {
+    myImg.current.src = "https://placehold.co/600x400/orange/white";
+    myImg.current.setAttribute('height', '200px');
+     myImg.current.setAttribute('width','300px')
 
+  }
+  const firstLastName = () => {
+    let fName = firstName.value;
+    let lName = lastName.value;
+    alert(fName + " " + lName);
+  }
+  const cssChange = () => {
+    myCss.current.classList.remove('text-success');
+    myCss.current.classList.add('text-danger');
+
+  }
   return (
     <div>
       <Hero item={ItemObj} btn={BtnClick} />
+      <div>
+        <h1 ref={myHeadline}></h1>
+        <img ref={ myImg} src="https://placehold.co/600x400" alt="" />
+        <button onClick={change}>Ref Click</button>
+          <button onClick={changeImg}>Ref Click image</button>
+
+      </div>
+      <div>
+        <input ref={(a) => firstName=a} type="text" placeholder='First NAme' name="" id="" />
+                <input ref={(b)=> lastName=b} type="text" placeholder='Last NAme' name="" id="" />
+<button onClick={firstLastName}>flname Click</button>
+      </div>
+      <h1 ref={myCss} className='text-success'>My name is Rashed
+      </h1>
+      <button onClick={cssChange}>change css</button>
     </div>
   );
 };
